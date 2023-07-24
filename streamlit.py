0cbd3d77dcc4f3fd06c6fa24d055e4665cc5f445
pc-ds-dev <rezkywahyudasitepu@gmail.com>
2023-07-24 20:55:33 +0700

    add streamlit ui for chatbot mode

---
+++ streamlit.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+st.title("💬 Chatbot (based on Llama 2)")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    print(st.session_state.messages)
+    response = requests.post('http://localhost:3398/chat', json={"text": st.session_state.messages}, timeout=60).json()
+    response = {"choices": [{
+        "message": {
+            "role": "assistant",
+            "content": response["result"]
+        }
+    }]}
+    msg = response["choices"][0]["message"]
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg["content"])
